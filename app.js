f6f66615bab2323b28fa39f52594fa56f9f89276
Elena Kuznetsova <elenakuznetsova177@yandex.ru>
2022-03-07 00:39:38 +0300

    app

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express()
 const PORT = 3000
 
 app.use(express.static(`${__dirname}/dist`))
+function serveIndev(req, res) {
+  res.redirect('/')
+}
+
+app.get('*', serveIndev)
 app.listen(PORT, () => {
   console.log(`Running on  http://localhost:${PORT}`)
 })
